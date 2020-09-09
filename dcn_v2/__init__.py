c45d740c906a0071e6fb6fbc3b10fdca4172bda6
Toraudonn <toraudonn@gmail.com>
2020-09-09 18:50:15 +0900

    add

---
+++ dcn_v2/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+__version__ = "0.0.1"
+
+
+from .dcn_v2 import *
---
+++ setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import codecs
+import glob
+import os
+import re
+
+try:
+    import torch
+except ImportError:
+    raise ImportError("Pytorch not found. Please install pytorch first.")
+
+from torch.utils.cpp_extension import BuildExtension
+from torch.utils.cpp_extension import CUDA_HOME
+from torch.utils.cpp_extension import CppExtension
+from torch.utils.cpp_extension import CUDAExtension
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+def get_extensions():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    extensions_dir = os.path.join(this_dir, "DCNv2", "src")
+
+    main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
+    source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
+    source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
+
+    os.environ["CC"] = "g++"
+    sources = main_file + source_cpu
+    extension = CppExtension
+    extra_compile_args = {"cxx": []}
+    define_macros = []
+
+    if torch.cuda.is_available() and CUDA_HOME is not None:
+        extension = CUDAExtension
+        sources += source_cuda
+        define_macros += [("WITH_CUDA", None)]
+        extra_compile_args["nvcc"] = [
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+        ]
+    else:
+        # raise NotImplementedError('Cuda is not available')
+        pass
+
+    sources = [os.path.join(extensions_dir, s) for s in sources]
+    include_dirs = [extensions_dir]
+    ext_modules = [
+        extension(
+            "dcn_v2.dcn_backend",
+            sources,
+            include_dirs=include_dirs,
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args,
+        )
+    ]
+    return ext_modules
+
+
+setup(
+    name="DCNv2",
+    version=find_version("dcn_v2", "__init__.py"),
+    author="haruya ishikawa",
+    url="",
+    description="deformable convolutional networks",
+    packages=find_packages(exclude=('tests')),
+    ext_modules=get_extensions(),
+    cmdclass={"build_ext": BuildExtension},
+)
